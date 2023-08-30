19346dc9f134760497044dcb771d90774be61f50
wjdgkwjd <wjdgkwjdw1@gmail.com>
2023-08-30 18:22:45 +0900

    update

---
+++ naverNews.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+html = urlopen("https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=226")
+
+soup = BeautifulSoup(html, "html.parser")
+news = soup.select("#main_content > div.list_body.newsflash_body > ul.type06_headline > li > dl > dt> a")
+n=0
+for  new in news:
+    url=new.get("href") 
+    new=new.text.strip()
+    if  new !="" :
+        n +=1
+        print( "{:02d}".format(n), new) #:02d는 2자리 수로 출력하라는 뜻
+        print( "   ", url)
+
+##main_content > div.list_body.newsflash_body > ul.type06_headline > li:nth-child(1) > dl > dt:nth-child(2) > a
